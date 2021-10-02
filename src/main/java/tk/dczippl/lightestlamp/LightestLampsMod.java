0bee7c940354f3a72e6375fdd75a36254ce856bd
DcZipPL <dczip89@gmail.com>
2021-10-02 21:40:52 +0200

    Removed empty classes

--- src/main/java/tk/dczippl/lightestlamp/LightestLampsMod.java
+++ src/main/java/tk/dczippl/lightestlamp/LightestLampsMod.java
@@ -71,98 +71,10 @@ public class LightestLampsMod implements ModInitializer
             });
         });
         
-        ModFluids.init();
+        //ModFluids.init();
         ModBlocks.init();
         ModItems.init();
         ModBlockEntities.init();
         WorldGenerator.register();
     }
-
-/*    @SubscribeEvent
-    public void onEntityLivingDeath(LivingDeathEvent event)
-    {
-        if (event.getSource().getDamageType().equals("player"))
-        {
-            if(event.getEntityLiving() instanceof IMob)
-            {
-                if (event.getEntityLiving().getEntityWorld().getMoonFactor() == 1.0F)
-                {
-                    Random rnd = new Random();
-                    if (rnd.nextInt(4) == 2)//25% chance
-                    event.getEntityLiving().entityDropItem(new ItemStack(ModItems.MOON_SHARD));
-                }
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public void EntityUpdate(LivingEvent.LivingUpdateEvent event)
-    {
-        LivingEntity entity = event.getEntityLiving();
-        boolean disabled = false;
-        if (entity instanceof PlayerEntity)
-        {
-            if (((PlayerEntity)entity).isSpectator())
-                disabled = true;
-        }
-        if (!disabled)
-        {
-            BlockPos pos = new BlockPos(entity.getPositionVec());
-            Block b = entity.getEntityWorld().getBlockState(pos.offset(Direction.UP)).getBlock();
-            Block b1 = entity.getEntityWorld().getBlockState(pos).getBlock();
-            if (b.equals(ModFluids.BROMINE_FLUID_BLOCK.get()))
-            {
-                entity.setMotion(new Vector3d(entity.getMotion().x, 0.100000011620D, entity.getMotion().z));
-            } else if (b1.equals(ModFluids.BROMINE_FLUID_BLOCK.get()))
-            {
-                entity.setMotion(new Vector3d(entity.getMotion().x, 0.100000011620D, entity.getMotion().z));
-            }
-        }
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void setFog(EntityViewRenderEvent.FogColors fog)
-    {
-        World w = fog.getInfo().getRenderViewEntity().getEntityWorld();
-        BlockPos pos = fog.getInfo().getBlockPos();
-        BlockState bs = w.getBlockState(pos);
-        Block b = bs.getBlock();
-        if(b.equals(ModFluids.BROMINE_FLUID_BLOCK.get()))
-        {
-            float red = 0.2F, green = 0.05F, blue = 0.05F;
-            fog.setRed(red); fog.setGreen(green); fog.setBlue(blue);
-
-        }
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void setFogDensity(EntityViewRenderEvent.FogDensity fog)
-    {
-        World w = fog.getInfo().getRenderViewEntity().getEntityWorld();
-        BlockPos pos = fog.getInfo().getBlockPos();
-        BlockState bs = w.getBlockState(pos);
-        Block b = bs.getBlock();
-        if(b.equals(ModFluids.BROMINE_FLUID_BLOCK.get()))
-        {
-            fog.setDensity(1f);
-        }
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void setFogLength(EntityViewRenderEvent.RenderFogEvent fog)
-    {
-        World w = fog.getInfo().getRenderViewEntity().getEntityWorld();
-        BlockPos pos = fog.getInfo().getBlockPos();
-        BlockState bs = w.getBlockState(pos);
-        Block b = bs.getBlock();
-        if(b.equals(ModFluids.BROMINE_FLUID_BLOCK.get()))
-        {
-            float progress = 36f / 30f;
-            RenderSystem.fogStart((1 - progress) * fog.getFarPlaneDistance() * .75f);
-            RenderSystem.fogEnd(fog.getFarPlaneDistance() * (1 - .8f * progress));
-        }
-    }*/
 }
