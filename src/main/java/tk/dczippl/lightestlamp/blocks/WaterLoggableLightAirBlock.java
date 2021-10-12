deca3ebab50b7e97370bcee88a243596633cb0bb
Prefex <dczip89@gmail.com>
2021-10-12 13:14:16 +0200

    Fix Water no more generates when placed underwater Lamps

--- src/main/java/tk/dczippl/lightestlamp/blocks/WaterLoggableLightAirBlock.java
+++ src/main/java/tk/dczippl/lightestlamp/blocks/WaterLoggableLightAirBlock.java
@@ -46,7 +46,7 @@ public class WaterLoggableLightAirBlock extends Block implements Waterloggable
     }
 
     public FluidState getFluidState(BlockState state) {
-        return Fluids.WATER.getStill(false);
+        return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
     }
 
     public boolean canFillWithFluid(BlockView world, BlockPos pos, BlockState state, Fluid fluid) {
