03591c8d42c2153a80452c45e533df4f604e1b9f
Kordian Kisieliński <dczip89@gmail.com>
2023-06-10 23:39:25 +0200

    Remove bromine to align with a Forge version

--- src/main/java/tk/dczippl/lightestlamp/init/ModMiscs.java
+++ src/main/java/tk/dczippl/lightestlamp/init/ModMiscs.java
@@ -8,16 +8,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import tk.dczippl.lightestlamp.machine.gascentrifuge.GasCentrifugeScreenHandler;
-import tk.dczippl.lightestlamp.potion.BrominePoison;
 
 import java.awt.*;
 
 import static tk.dczippl.lightestlamp.LightestLampsMod.MOD_ID;
 
 public class ModMiscs {
-	public static final StatusEffect BROMINE_POISON = new BrominePoison(StatusEffectCategory.HARMFUL,new Color(102,16,0).getRGB());
-	//public static final DamageSource BROMINE = new DamageSource("bromine").setDamageBypassesArmor();
-
 	public static final ItemGroup LAMPS_TAB = FabricItemGroupBuilder.build(new Identifier(MOD_ID,"lamps"),()->new ItemStack(ModBlocks.OMEGA_LAMP,16));
 	public static final ScreenHandlerType<GasCentrifugeScreenHandler> CENTRIFUGE_SH = ScreenHandlerRegistry.registerExtended(new Identifier("glowstone_centrifuge"),GasCentrifugeScreenHandler::new);
 }
