e558d03d8805ba5b39bfe88968b2ef8c7dcc9c16
Kordian Kisieliński <dczip89@gmail.com>
2023-08-17 19:30:20 +0200

    Fix "Bowl and Stick" crafting item, use standard registration method for block entities.

--- src/main/java/tk/dczippl/lightestlamp/init/ModBlockEntities.java
+++ src/main/java/tk/dczippl/lightestlamp/init/ModBlockEntities.java
@@ -4,7 +4,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityT
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import tk.dczippl.lightestlamp.LightestLampsMod;
+import net.minecraft.util.Identifier;
 import tk.dczippl.lightestlamp.blocks.GenerableLampBlock;
 import tk.dczippl.lightestlamp.blocks.entity.AlchemicalLampBlockEntity;
 import tk.dczippl.lightestlamp.blocks.entity.ChunkCleanerBlockEntity;
@@ -12,18 +12,20 @@ import tk.dczippl.lightestlamp.blocks.entity.GenerableLampBlockEntity;
 import tk.dczippl.lightestlamp.blocks.entity.OmegaChunkCleanerBlockEntity;
 import tk.dczippl.lightestlamp.machine.gascentrifuge.GasCentrifugeBlockEntity;
 
+import static tk.dczippl.lightestlamp.LightestLampsMod.MOD_ID;
+
 public class ModBlockEntities
 {
-	public static final BlockEntityType<GenerableLampBlockEntity> GENERAL_LAMP_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, LightestLampsMod.MOD_ID+":general_lamp", FabricBlockEntityTypeBuilder.create(
+	public static final BlockEntityType<GenerableLampBlockEntity> GENERAL_LAMP_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "general_lamp"), FabricBlockEntityTypeBuilder.create(
 	        (pos,state)->new GenerableLampBlockEntity(((GenerableLampBlock)state.getBlock()).lampType,((GenerableLampBlock)state.getBlock()).waterResistant,pos,state),
             ModBlocks.ALPHA_LAMP,ModBlocks.BETA_LAMP,ModBlocks.GAMMA_LAMP,ModBlocks.DELTA_LAMP,ModBlocks.EPSILON_LAMP,ModBlocks.ZETA_LAMP,ModBlocks.ETA_LAMP,ModBlocks.OMEGA_LAMP,
             ModBlocks.CLEAR_SEA_LANTERN,ModBlocks.DEEP_SEA_LANTERN,ModBlocks.OCEAN_LANTERN,ModBlocks.DEEP_OCEAN_LANTERN,ModBlocks.ABYSSAL_LANTERN
     ).build());
-    public static BlockEntityType<ChunkCleanerBlockEntity> CC_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, LightestLampsMod.MOD_ID+":cc",FabricBlockEntityTypeBuilder.create(ChunkCleanerBlockEntity::new,ModBlocks.CHUNK_CLEANER).build());
-    public static BlockEntityType<OmegaChunkCleanerBlockEntity> OCC_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, LightestLampsMod.MOD_ID+":occ",FabricBlockEntityTypeBuilder.create(OmegaChunkCleanerBlockEntity::new,ModBlocks.OCC).build());
+    public static BlockEntityType<ChunkCleanerBlockEntity> CC_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "cc"),FabricBlockEntityTypeBuilder.create(ChunkCleanerBlockEntity::new,ModBlocks.CHUNK_CLEANER).build());
+    public static BlockEntityType<OmegaChunkCleanerBlockEntity> OCC_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "occ"),FabricBlockEntityTypeBuilder.create(OmegaChunkCleanerBlockEntity::new,ModBlocks.OCC).build());
 
-    public static BlockEntityType<AlchemicalLampBlockEntity> ALCHEMICALLAMP_TE = Registry.register(Registries.BLOCK_ENTITY_TYPE, LightestLampsMod.MOD_ID+":alchemical_lamp",FabricBlockEntityTypeBuilder.create(AlchemicalLampBlockEntity::new,ModBlocks.ALCHEMICAL_LAMP).build());
-    public static BlockEntityType<GasCentrifugeBlockEntity> CENTRIFUGE_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, LightestLampsMod.MOD_ID+":glowstone_centrifuge",FabricBlockEntityTypeBuilder.create(GasCentrifugeBlockEntity::new,ModBlocks.GLOWSTONE_CENTRIFUGE).build());
+    public static BlockEntityType<AlchemicalLampBlockEntity> ALCHEMICALLAMP_TE = Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "alchemical_lamp"),FabricBlockEntityTypeBuilder.create(AlchemicalLampBlockEntity::new,ModBlocks.ALCHEMICAL_LAMP).build());
+    public static BlockEntityType<GasCentrifugeBlockEntity> CENTRIFUGE_BE = Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "glowstone_centrifuge"),FabricBlockEntityTypeBuilder.create(GasCentrifugeBlockEntity::new,ModBlocks.GLOWSTONE_CENTRIFUGE).build());
 
     public static void init() {
     }
--- src/main/java/tk/dczippl/lightestlamp/items/StickAndBowlItem.java
+++ src/main/java/tk/dczippl/lightestlamp/items/StickAndBowlItem.java
@@ -17,15 +17,14 @@ public class StickAndBowlItem extends Item
         return true;
     }
 
-    /*@Override
-    public ItemStack getContainerItem(ItemStack itemStack)
-    {
+    @Override
+    public ItemStack getRecipeRemainder(ItemStack itemStack) {
         ItemStack stack = itemStack.copy();
         if (stack.getDamage() != stack.getMaxDamage())
             stack.setDamage(stack.getDamage()+1);
         else stack = new ItemStack(Items.BOWL);
         return stack;
-    }*/ // TODO: Check if it work
+    }
 
     @Override
     public boolean hasRecipeRemainder() {
